ddaae7726ad8b456ff609f0c049d5a58f3061c93
Lust3r <s1skyblue1s@gmail.com>
2023-03-06 10:26:22 +0900

    [feat] 고객 수를 계산하는 Counter class 생성

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -9,18 +9,17 @@ import Foundation
 
 struct Bank: CustomerManageable {
 
+    let todayCounter = Counter()
     let accountBanker = Banker(processingTime: 0.7)
     let loanBanker = Banker(processingTime: 1.1)
 
     func open() {
-        
         let numberOfTodayCustomers = makeAcceptableNumber()
         let customerQueue = makeCustomerQueue(maxCount: numberOfTodayCustomers)
+
         let loanSemaphore = DispatchSemaphore(value: 1)
         let accountSemaphore = DispatchSemaphore(value: 2)
         let group = DispatchGroup()
-        var numberOfAccountCustomers = 0
-        var numberOfloanCustomers = 0
 
         while !customerQueue.isEmpty() {
             guard let currentCustomer = customerQueue.dequeue() else { return }
@@ -31,28 +30,27 @@ struct Bank: CustomerManageable {
                 DispatchQueue.global().async(group: group) {
                     accountSemaphore.wait()
                     accountBanker.work(for: currentCustomer.waitingOrder, what: currentCustomerWorkType)
-                    numberOfAccountCustomers += 1
+                    todayCounter.addAccountCustomer(with: 1)
                     accountSemaphore.signal()
                 }
             } else {
                 DispatchQueue.global().async(group: group) {
                     loanSemaphore.wait()
                     loanBanker.work(for: currentCustomer.waitingOrder, what: currentCustomerWorkType)
-                    numberOfloanCustomers += 1
+                    todayCounter.addLoanCustomer(with: 1)
                     loanSemaphore.signal()
                 }
             }
         }
 
         group.wait()
-
-        let totalWorkTime = calculateTotalWorkTime(customers: numberOfAccountCustomers, numberOfloanCustomers, processingTimes: accountBanker.processingTime, loanBanker.processingTime)
-
-        showWorkFinishMessage(numberOfTodayCustomers, totalWorkTime)
+        showWorkFinishMessage(numberOfTodayCustomers)
+        todayCounter.resetCounter()
     }
 
-    func showWorkFinishMessage(_ totalNumber: Int, _ totalTime: Double) {
-        let totalWorkTime = String(format: "%.2f", totalTime)
+    func showWorkFinishMessage(_ totalNumber: Int) {
+        let calculateWorkTime = calculateTotalWorkTime(customers: todayCounter.getAccountCustomerCount(), todayCounter.getLoanCustomerCount(), processingTimes: accountBanker.processingTime, loanBanker.processingTime)
+        let totalWorkTime = String(format: "%.2f", calculateWorkTime)
 
         print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalNumber)명이며, 총 업무시간은 \(totalWorkTime)초입니다.")
     }
@@ -61,7 +59,7 @@ struct Bank: CustomerManageable {
         var result = 0.0
 
         customers.enumerated().forEach {
-             let tempResult = Double($1) * processingTimes[$0]
+            let tempResult = Double($1) * processingTimes[$0]
             result += tempResult
         }
 
